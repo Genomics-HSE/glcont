8c7cb9037f7320dec79a65323d2af1196e8b9ad4
Nikita <nikitaminecraft@gmail.com>
2025-09-17 00:41:19 +0300

    update calc_stats

--- new_bams/calc_stats.sh
+++ new_bams/calc_stats.sh
@@ -5,25 +5,39 @@ set -euo pipefail
 
 PAIRS_TXT="$1"
 folder=$(basename "$PAIRS_TXT" .samples.txt )
-tail "$PAIRS_TXT" | while IFS=, read -r loc1 sample1 loc2 sample2
-do
-    contaminant_file="all_consensuses/${sample2}.final.fa"
+
+# Create a function for the inner loop
+process_combination() {
+    local sample1="$1"
+    local sample2="$2"
+    local h="$3"
+    local p="$4"
+    local v="$5"
+    local model="$6"
+    local consensus="$7"
+    local folder="$8"
+    
+    local contaminant_file="all_consensuses/${sample2}.final.fa"
+    local bam="${folder}/${sample1}_${sample2}_cov${h}_p${p}_v${v}.bam"
+    
+    sh glcont.sh ../rCRS.fa "$bam" -c "$contaminant_file" -s "$consensus" -m "$model"
+}
+
+export -f process_combination
+
+# Generate all parameter combinations and feed to parallel
+tail "$PAIRS_TXT" | while IFS=, read -r loc1 sample1 loc2 sample2; do
     for h in 5 10 30; do
         for p in 50 60 70 80 90 95; do
             for v in {0..2}; do
                 for model in 0 1; do
                     for consensus in all_consensuses/${sample1}.final.fa mpileup; do
-                        bam=${folder}/${sample1}_${sample2}_cov${h}_p${p}_v${v}.bam
-                        
-                        sh glcont.sh ../rCRS.fa "$bam" -c "$contaminant_file" -s "$consensus" -m $model
-                        exit 0
-                        # if [[ "$consensus" != "mpileup" ]]; then
-    #                         consensus="True"
-    #                     fi
-    #                     echo "$sample1,$sample2,$h,$p,$v,$model,$consensus,$P"
+                        echo "$sample1 $sample2 $h $p $v $model $consensus $folder"
                     done
                 done
             done
         done
     done
-done
+done | parallel -j $(nproc) --colsep ' ' '
+    process_combination {1} {2} {3} {4} {5} {6} {7} {8}
+'
